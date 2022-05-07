db8efa6efb9773ee66b9d236a4145cdf86e2f246
yusufturaev707 <yusufturaev707@gmail.com>
2022-05-07 07:58:45 +0500

    reset password  commit

--- config/settings.py
+++ config/settings.py
@@ -98,3 +98,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'user_app.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from .views import login_page, logout_user, register_page, profile_page, settings_page, change_password, edit_profile
+from django.contrib.auth import views as auth_views
+from .views import login_page, logout_user, register_page, profile_page, settings_page, change_password, edit_profile, \
+    password_reset
 
 urlpatterns = [
     path('', profile_page, name='profile_page'),
@@ -10,4 +12,15 @@ urlpatterns = [
     path('register/', register_page, name='register'),
     path('change_password/', change_password, name='change_password'),
     path('edit_profile/', edit_profile, name='edit_profile'),
+
+    path('password_reset/', password_reset, name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='user_app/register/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="user_app/register/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='user_app/register/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
